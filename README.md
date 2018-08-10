da0fcabae0768ab3c997dea6f4a784d9ec987d06
ReneSac <reneduani@yahoo.com.br>
2018-08-10 19:56:54 -0300

    Add some documentation.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-Collection of aliases and procedures that make easier to write C in nim. 
+Collection of aliases and procedures that make easier to write C in nim. ;)
 
-Includes pointer arithmetic, some useful bitops (most were removed as we got the very nice bitops module in nim standard library!), and some misc aliases for shallowCopy.
+Contains many aliases for the nim's keyword bitwise operators. 
+
+Nim's keyword operators don't support nim's strong-spaces feature, nor have an
+assigment operator version (like `+=`). The reuse of the logical keywords for
+`and`, `or` and `not` for the bitwise version also hinders readability somewhat
+and have arguably the wrong precedence. This module attempts to fix all that.
+
+It also includes pointer arithmetic, some useful bitops (most were removed as
+we got the very nice bitops module in nim standard library!), and some misc 
+aliases for shallowCopy.
--- src/c_alikes.nim
+++ src/c_alikes.nim
@@ -1,65 +1,73 @@
 #? strongSpaces
 
-
-#converter toUint8*(x: int{lit}):uint8 = x.uint8
-#converter toUint16*(x: int{lit}):uint16 = x.uint16
-#converter toUint32*(x: int{lit}):uint32 = x.uint32
-#converter toUint64*(x: int{lit}):uint64 = x.uint64
-
-#proc `shl` *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x shl T(y)
-#proc `shr` *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x shr T(y)
-#proc `or`  *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x or T(y)
-#proc `and` *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x and T(y)
-#proc `xor` *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x xor T(y)
-#proc `div` *[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = x div T(y)
-
 proc `*<`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} =
-  ## Shift left, equivalent to C's `<<`. We can't use the same operator in nim
-  ## so this *< is the best compromise, as the precedence is also correct.
+  ## Shift left, equivalent to C's ``<<``. We can't use the same operator in nim
+  ## so this ``*<`` is the best compromise, as the precedence is also correct.
   x shl y
 
 proc `*>`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} =
-  ## Shift right, equivalent to C's `>>` We can't use the same operator in nim
-  ## so this *< is the best compromise, as the precedence is also correct.
+  ## Shift right, equivalent to C's ``>>`` We can't use the same operator in nim
+  ## so this ``*<`` is the best compromise, as the precedence is also correct.
   x shr y
 
 proc `|`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = 
   ## Bitwise OR. Due to Nim's precedence rules, it has higher precedence than
-  ## `&`, the same precedence as `+` and `-`, unlike C.  
+  ## ``&``, the same precedence as ``+`` and ``-``, unlike C.  
   x or y
 
 proc `&`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = 
   ## Bitwise AND. Due to Nim's precedence rules, it has lower precedence than
-  ## `|` and `|^`. At least it has higher precedence than logical `and`, `or`,
-  ## `..` and all comparison operators, unlike C.
+  ## ``|`` and ``|^``. At least it has higher precedence than logical ``and``, ``or``,
+  ## ``..`` and all comparison operators, unlike C.
   x and y
 
 proc `|^`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = 
-  ## Bitwise XOR. `^` was already taken in nim. The `|` in the front ensures equal
-  ## precedence to the simple `|`.
+  ## Bitwise XOR. ``^`` was already taken in nim. The ``|`` in the front ensures equal
+  ## precedence to the simple ``|``.
   x xor y
 
 proc `~`*[T:SomeInteger](x: T): T {.inline, noSideEffect.} = 
-  ## Bitwise NOT.
+  ## Bitwise NOT. Alias for nim's ``not`` operator.
   not x
 
 proc `//`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} = 
-  ## Integer division.
+  ## Integer division. Alias for nim's ``div`` operator.
   x div y
 
 proc `%`*[T,U: SomeInteger](x:T, y:U): T {.inline, noSideEffect.} =
-  ## Integer modulo.
+  ## Integer modulo. Alias for nim's ``mod`` operator.
   x mod y
-  
-# Increment and decrement operators from C
-proc `++`*(x: var SomeInteger): SomeInteger {.inline, discardable.} = inc(x); return x
-proc `--`*(x: var SomeInteger): SomeInteger {.inline, discardable.} = dec(x); return x
 
-# Equivalent to the C postfix operators, but are prefix operators too, as
-# Nim don't allows postfix operators. If you want to discard the result, use
-# the above operators.
-proc `+++`*(x: var SomeInteger): SomeInteger {.inline.} = result = x; inc(x)
-proc `---`*(x: var SomeInteger): SomeInteger {.inline.} = result = x; dec(x)
+
+# Increment and decrement operators from C
+
+proc `++`*(x: var SomeInteger): SomeInteger {.inline, discardable.} =
+  ## Increment `x` and return the incremented value. Discardable.
+  ## Same as ``++x`` pre-increment in C.
+  inc(x)
+  result = x
+
+proc `--`*(x: var SomeInteger): SomeInteger {.inline, discardable.} =
+  ## Decrement `x` and return the decremented value. Discardable.
+  ## Same as ``--x`` pre-decrement in C.
+  dec(x)
+  result = x
+
+proc `+++`*(x: var SomeInteger): SomeInteger {.inline.} =
+  ## Increment `x` but return the value before the increment.
+  ## Same as ``x++`` post-increment in C, but a prefix operator, as nim doesn't
+  ## support postfix operators.
+  ## If you want to discard the returned value, use the pre-increment version.
+  result = x
+  inc(x)
+
+proc `---`*(x: var SomeInteger): SomeInteger {.inline.} = 
+  ## Decrement `x` but return the value before the decrement.
+  ## Same as ``x--`` post-decrement in C, but a prefix operator, as nim doesn't
+  ## support postfix operators.
+  ## If you want to discard the returned value, use the pre-decrement version.
+  result = x
+  dec(x)
 
 # Assignment Operators:
 
@@ -107,105 +115,134 @@ proc toggleBit*[T:SomeInteger](x: var T, pos: Natural) {.inline, noSideEffect.} 
   x |^= cast[T](1 *< pos)
 
 proc isBitSet*[T:SomeInteger](x: var T, pos: Natural): bool {.inline, noSideEffect.} =
-  ## Test the bit at the position `pos` of `x`
+  ## Test the bit at the position `pos` of `x`.
   x & (1 *< pos) > 0
 
 
 
-proc bitSizeof*(t:typedesc[SomeInteger]): int8 =
-  ## Returns the size in bits of a given type ``t``.
-  ## Supports only types whose ``sizeof()`` is less than 32 due to it's
-  ## output type.
+proc bitSizeof*(t: typedesc): int =
+  ## Returns the size in bits of a given type `t`.
+  ##
   ## .. code-block:: Nim
   ##   assert int32.bitLenght == 32
-  ##   assert .bitLenght == 1
-  assert(t.sizeof < 32)
   t.sizeof * 8
 
-proc bitLenght*(x: SomeInteger): int8 =
+proc bitLenght*(x: SomeInteger): int =
   ## Returns the minimum number of bits needed to store a given positive value.
-  ## Will return 1 if x == 0.
+  ## Will return 1 if ``x == 0``. 
+  ## For that the use of the `noUndefinedBitOps` flag is required.
+  ##
   ## .. code-block:: Nim
   ##   assert 1.bitLenght == 1
   ##   assert 0.bitLenght == 1
   ##   assert 3.bitLenght == 2
   ##   assert 4.bitLenght == 3
-  x.bitSizeof - x.countLZ + ord(x == 0)
+  x.bitSizeof - x.countLeadingZeroBits + ord(x == 0)
 
 # Adapted from Jehan: http://forum.nim-lang.org/t/1188
 
 # Addition and subtraction on pointers (pointer arithmetic)
 template `+`*[T](p: ptr T, off: SomeInteger): ptr T =
+  ## Addition on pointer. Same as in C.
   cast[ptr type(p[])](cast[ByteAddress](p) +% off.int * sizeof(p[]))
 
 template `+=`*[T](p: ptr T, off: SomeInteger) =
+  ## Addition on pointer. Same as in C.
   p = p + off
 
 template `-`*[T](p: ptr T, off: SomeInteger): ptr T =
+  ## Subtraction on pointer. Same as in C.
   cast[ptr type(p[])](cast[ByteAddress](p) -% off.int * sizeof(p[]))
 
 template `-=`*[T](p: ptr T, off: SomeInteger) =
+  ## Subtraction on pointer. Same as in C.
   p = p - off
 
 # Subtraction to get the difference between pointers
 proc `-`*[T](a, b: ptr T): int {.inline.} =
-  # Distance between pointed elements, in multiples of that element size.
-  #
-  # If the size of the pointed elements is 1 byte and they are more than 2GB
-  # apart in a 32 bit machine, the result will be bogus (as it probably will be
-  # in C too).
-  #
-  # There is no similar gotcha for 64 bits as the addressing space for the
-  # forseable future is limited to well bellow 63 bits.
+  ## Distance between pointed elements, in multiples of that element size.
+  ##
+  ## If the size of the pointed elements is 1 byte and they are more than 2GB
+  ## apart in a 32 bit machine, the result will be bogus (as it probably will be
+  ## in C too).
+  ##
+  ## There is no similar gotcha for 64 bits as the addressing space for the
+  ## forseable future is limited to well bellow 63 bits.
   cast[int](cast[uint](a) div sizeof(T).uint) -
     cast[int](cast[uint](b) div sizeof(T).uint)
 
 # Indexing directly on pointers
 template `[]`*[T](p: ptr T, off: int): T =
+  ## Indexing like arrays directly on pointers
   (p + off)[]
 
 template `[]=`*[T](p: ptr T, off: int, val: T) =
+  ## Indexing assigment like arrays directly on pointers
   (p + off)[] = val
 
 # Nim style increment and decrement of pointers
 template inc*[T](p: ptr T, off = 1) =
+  ## Nim style increment for pointers.
   p = p + off
 
 template dec*[T](p: ptr T, off = 1) =
+  ## Nim style decrement for pointers.
   p = p - off
 
 # Pre and post increments and decrements on pointers
 proc `++`*[T](p: var ptr T): ptr T {.inline, discardable.} =
+  ## Increment pointer `p` in-place and return the incremented pointer.
+  ## Discardable.
+  ## Same as ``++p`` pointer pre-increment in C.
   p = p + 1
   result = p
 
 proc `--`*[T](p: var ptr T): ptr T {.inline, discardable.} =
+  ## Decrement pointer `p` in-place and return the decremented pointer.
+  ## Discardable.
+  ## Same as ``++p`` pointer pre-decrement in C.
   p = p - 1
   result = p
 
 proc `+++`*[T](p: var ptr T): ptr T {.inline.} =
+  ## Increment pointer `p` in-place but return the pointer
+  ## before the increment.
+  ## Discardable.
+  ## Same as ``p++`` pointer post-increment in C.
   result = p
   p = p + 1
 
 proc `---`*[T](p: var ptr T): ptr T {.inline.} =
+  ## Decrement pointer `p` in-place but return the pointer
+  ## before the decrement.
+  ## Discardable.
+  ## Same as ``p++`` pointer post-decrement in C.
   result = p
   p = p - 1
 
 # The same as above, but deferencing the resulting pointer
 proc `*++`*[T](p: var ptr T): var T {.inline.} =
-  # Same as *++p in C
+  ## First increment pointer `p` in place and then deference it, 
+  ## returning a mutable reference to the pointed value.
+  ## Same as ``*++p`` in C.
   result = (++p)[]
 
 proc `*--`*[T](p: var ptr T): var T {.inline.} =
-  ## Same as *--p in C
+  ## First decrement pointer `p` in place and then deference it, 
+  ## returning a mutable reference to the pointed value.
+  ## Same as ``*--p`` in C.
   result = (--p)[]
 
 proc `*+++`*[T](p: var ptr T): var T {.inline.} =
-  ## Same as *p++ in C
+  ## First deference pointer `p`, returning a mutable reference to
+  ## the pointed value, and then increment the pointer in place.
+  ## Same as ``*p++`` in C.
   result = (+++p)[]
 
 proc `*---`*[T](p: var ptr T): var T {.inline.} =
-  ## Same as *p-- in C
+  ## First deference pointer `p`, returning a mutable reference to
+  ## the pointed value, and then decrement the pointer in place.
+  ## Same as ``*p--`` in C.
   result = (---p)[]
 
 proc testPtrMath() =
@@ -224,7 +261,8 @@ template cfor*(preLoop, condition, postLoop, body: untyped) =
   ## C style for loop. It is useful over the normal for when you need to
   ## change the loop counter inside the loop, for example.
   ## To see what exactly it does, look at the source. It is beautifully simple.
-  ## Use with #! strongSpaces so you don't need parenthesis around all the arguments.
+  ##
+  ## Use with `#? strongSpaces` so you don't need parenthesis around all the arguments.
   ## Use a () around the first argument to transform it in an expression.
   ## For example: (var i = 0; c = 1) or (discard).
   block:
@@ -233,11 +271,10 @@ template cfor*(preLoop, condition, postLoop, body: untyped) =
       body
       postLoop
 
-
 # Not really c-alike, but useful templates from Jehan
 # https://forum.nim-lang.org/t/1543#9665
 template `<-`*[T](target, source: var T) =
-  ## Shallow copies from source to target. Similar to C++ move semantics. See
+  ## Shallow copies from `source` to `target`. Similar to C++ move semantics. See
   ## the nim documentation about `shallowCopy()`.
   shallowCopy target, source
 
@@ -247,11 +284,18 @@ template alias*[T](varname: untyped, value: var T) =
   var varname {.inject.}: type(value)
   shallowCopy varname, value
 
-# toHex with pretty printing
 proc toHex*[T](x: T, len: int = -1, pretty: bool = false, prefix: bool = false,
-               sep: char = '\0', sepInterval: Positive = 4): string {.noSideEffect.} =
+               sep: char = '\0'): string {.noSideEffect.} =
+  ## Same as the toHex from strutils, but with optional pretty printing.
+  ## By default, when pretty printing, it uses ``_`` as the separator
+  ## each 4 hex digits.
+  ##
+  ## .. code-block:: Nim
+  ##   let h = 0xDEAD_BEEF'u32
+  ##   doAssert h.toHex(pretty=true) == "0xDEAD_BEEF"
   const HexChars = "0123456789ABCDEF"
-  var shift: T = 0
+  let casted = cast[int64](x)
+  var shift = 0'i64
   var iprefix = prefix
   var isep = sep
 
@@ -261,7 +305,7 @@ proc toHex*[T](x: T, len: int = -1, pretty: bool = false, prefix: bool = false,
       isep = '_'
 
   var resLen = if len < 0: T.sizeof * 2 else: len
-  let sepPadding = if isep != '\0': (resLen//sepInterval - 1) else: 0
+  let sepPadding = if isep != '\0': (resLen//4 - 1) else: 0
   let prefixPadding = if iprefix: 2 else: 0
   resLen += sepPadding + prefixPadding
   result = newString(resLen)
@@ -273,7 +317,7 @@ proc toHex*[T](x: T, len: int = -1, pretty: bool = false, prefix: bool = false,
     if shift % 16 == 0 and shift > 0 and result[j+1] != isep:
       result[j] = isep
       continue
-    result[j] = HexChars[toU32(x *> shift) & 0xF'i32]
+    result[j] = HexChars[int(casted *> shift & 0xF)]
     shift += 4
 
 
